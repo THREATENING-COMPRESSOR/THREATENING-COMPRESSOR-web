bf3c6ecc14258e6fcaf56dffa61427953cffc00d
Robby <gavigan.4@osu.edu>
2023-12-03 20:05:37 -0500

    Update the Web Frontend to Call the API

--- frontend/src/Products/Products.tsx
+++ frontend/src/Products/Products.tsx
@@ -1,10 +1,49 @@
-import data from "./data";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { type } from "os";
+import { error } from "console";
+
+// defining product type
+type Product = {
+    id: number;
+    name: string;
+    description: string;
+    brand: string;
+    price: number;
+    rating: number;
+    numberOfReviews: number;
+    imageUrl: string;
+}
+
+const emptyProducts: Product[] = [];
 
 function Products() {
+
+    // initializing empty array of products state hook
+    const [products, setProducts]: [Product[], (products: Product[]) => void]
+        = useState(emptyProducts);
+
+    // effect hook
+    // calls api and puts response into statehook's setProducts function
+    useEffect(() => {
+        
+        // We are feeding the HTTP Response Body- response.data- into our setProducts function from our state hook.
+        axios.get<Product[]>("https://localhost:5283/catalog",
+            {
+                headers: {
+                    "Content-Type": "application/json",
+                },
+            })
+            .then((response) => setProducts(response.data))
+            .catch((error) => console.log(error));
+    }, []);
+
     return (
         <div className="content">
             <ul className="products">
-                {data.products.map((product) => (
+                
+                {/* refers to api db info */}
+                {products.map((product) => (
                     <li>
                     <div className="product">
                         <img
