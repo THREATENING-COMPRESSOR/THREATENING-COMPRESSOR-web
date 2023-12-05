090a8897591a31784be7e1d6997fad7a6617f8e4
Robby <gavigan.4@osu.edu>
2023-12-05 17:32:26 -0500

    fixed issue where api wouldnt connect

--- frontend/src/Products/Products.tsx
+++ frontend/src/Products/Products.tsx
@@ -28,7 +28,7 @@ function Products() {
     useEffect(() => {
         
         // We are feeding the HTTP Response Body- response.data- into our setProducts function from our state hook.
-        axios.get<Product[]>("https://localhost:5283/catalog",
+        axios.get<Product[]>("http://localhost:5283/catalog",
             {
                 headers: {
                     "Content-Type": "application/json",
